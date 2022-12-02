267f9d8fc486638ff18813cef595bfee30a2e502
stewie-gil <gilliganstewie7@gmail.com>
2022-12-02 05:57:09 -0800

    m

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    ac = len(sys.argv) - 1    
-    res = 0    
-    if ac > 0:        
-        for i in range(ac):            
-            res += int(sys.argv[i + 1])            
+    ac = len(sys.argv) - 1
+    res = 0
+    if ac > 0:
+        for i in range(ac):
+            res += int(sys.argv[i + 1])
     print("{:d}".format(res))
