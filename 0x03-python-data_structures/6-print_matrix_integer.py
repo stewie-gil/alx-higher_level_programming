7ec1a7a926ee4b0addd65f70d60bcd55cac6262d
stewie-gil <gilliganstewie7@gmail.com>
2022-12-04 07:24:20 -0800

    m

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for col in row:
                 print("{} ".format(col), end='')
-            print("\n")
+            print()
