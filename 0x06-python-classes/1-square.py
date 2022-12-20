7d6ff4fba8c954caaa4ab9ea011b278663515d1d
stewie-gil <gilliganstewie7@gmail.com>
2022-12-20 14:18:34 -0800

    added 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module
+
+this module contains a class that defines a square
+"""
+
+
+class Square:
+
+    """Defines a square """
+
+    def __init__(self, size):
+        self.__size = size
