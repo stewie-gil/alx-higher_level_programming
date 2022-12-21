230db7e5fdfd8cc8bb8d625e95aad619805cd927
stewie-gil <gilliganstewie7@gmail.com>
2022-12-20 17:28:50 -0800

    m

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""Square module"""
+
+"""Square square."""
 
 
-class Square:
-    "Defines a square"
-    def area(self):
-        return self.__size**2
+class Square():
+    """Defines a square."""
 
     def __init__(self, size=0):
-        self.__size = size
-
+        """Sets the necessary attributes for the Square object.
+        Args:
+            size (int): the size of one edge of the square.
+        """
+        self.size = size
         @property
     def size(self):
+        """Get or set the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        self.__size = value
-        if type(self.__size) != int:
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        if self.__size < 0:
-            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
