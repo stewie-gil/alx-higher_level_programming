cfb16548d834a0c1e082e6b05046faaa2bdb2b8d
stewie-gil <gilliganstewie7@gmail.com>
2023-02-15 13:07:55 -0800

    SQL QUERIES

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- creates the table force_name on the server
-CREATE TABLE IF NOT EXISTS 'force_name'(
-'id' INT,
-'name' VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS force_name(
+id INT,
+name VARCHAR(256) NOT NULL
 );
 
