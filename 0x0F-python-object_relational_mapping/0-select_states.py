0355a3e74109866f2903607f44cc1e2e4e7883f0
stewie-gil <gilliganstewie7@gmail.com>
2023-03-19 12:49:59 -0700

    resolved pycodestyle issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" 
+"""
 Script that lists all states from the database hbt
-
 Arguements:
   mysl username (str)
   mysql password (str)
@@ -11,12 +10,12 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
+    u_nme = sys.argv[1]
+    pss = sys.argv[2]
+    db_name = sys.argv[3]
 
-    #by default it will connect to localhost:3306 but will still specify 
-    db = MySQLdb.connect(host = "localhost", port = 3306, user = username , password = password, database = database_name)
+    # by default it will connect to localhost:3306
+    db = MySQLdb.connect(user=u_nme, password=pss, database=db_name)
     cur = db.cursor()
 
     cur.execute("""SELECT * FROM states ORDER BY id""")
