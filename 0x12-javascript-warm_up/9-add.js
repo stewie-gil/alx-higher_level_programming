2e1a96a6dcb0684e79d80b0c8195a30bda606e35
stewie-gil <gilliganstewie7@gmail.com>
2023-03-26 01:36:03 -0700

    add function ;

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// prints addition of two ints
+
+function add (a, b) {
+  console.log(a + b);
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+add(a, b);
