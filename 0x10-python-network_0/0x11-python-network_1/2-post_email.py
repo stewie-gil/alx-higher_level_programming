90bc40319dec16981360ffed8a323e2a56bfa2c9
stewie-gil <gilliganstewie7@gmail.com>
2023-04-03 11:55:29 -0700

    add to 4th task

---
+++ 0x10-python-network_0/0x11-python-network_1/2-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python takes uRL and email)"""
+
+from urllib import request, parse
+from sys import argv
+
+if __name__ == "__main__":
+    data = parse.urlencode({"email": argv[2]}).encode('utf-8')
+    with request.urlopen(argv[1], data) as page:
+        print(page.read().decode('utf-8'))
