7412c3e9dde4c6ae510a6c25ea9c1f11667dcff2
stewie-gil <gilliganstewie7@gmail.com>
2023-04-25 16:44:18 -0700

    number tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,17 @@
 #!/usr/bin/node
 
-const request = require('request');
-const filmUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const movieId = prompt("Enter the movie ID:");
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-// Make a GET request to the SWAPI API to retrieve the film information
-request.get(filmUrl, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    // Parse the film information as a JSON object and retrieve the list of characters
-    const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
-
-    // Make a GET request to each character URL and print their name
-    characterUrls.forEach(characterUrl => {
-      request.get(characterUrl, (err, res, body) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
-      });
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const characters = data.characters;
+    characters.forEach(characterUrl => {
+      fetch(characterUrl)
+        .then(response => response.json())
+        .then(data => console.log(data.name))
+        .catch(error => console.error(error));
     });
-  }
-});
+  })
+  .catch(error => console.error(error));
