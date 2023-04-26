2e1640d75fd1fb436709ba91e1234d0228577749
stewie-gil <gilliganstewie7@gmail.com>
2023-04-25 17:10:55 -0700

    request

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,31 @@
 #!/usr/bin/node
-
 const request = require('request');
+const apiUrl = process.argv[2];
 
-/**
- * This function counts the number of completed tasks for each user in a given
- * list of tasks obtained from an API endpoint.
- * @param {string} apiUrl - The URL of the API endpoint.
- */
-function countCompletedTasksByUser(apiUrl) {
-  request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(`An error occurred while making the request: ${error}`);
-    } else {
-      const completedTasksByUser = {};
-      const tasks = JSON.parse(body);
-      for (const task of tasks) {
-        if (task.completed) {
-          if (!completedTasksByUser[task.userId]) {
-            completedTasksByUser[task.userId] = 1;
-          } else {
-            completedTasksByUser[task.userId] += 1;
-          }
-        }
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.log('Error:', response.statusCode);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach(function (todo) {
+    if (todo.completed) {
+      if (completedTasksByUser[todo.userId]) {
+        completedTasksByUser[todo.userId]++;
+      } else {
+        completedTasksByUser[todo.userId] = 1;
       }
-      console.log(completedTasksByUser);
     }
   });
-}
 
+  console.log(completedTasksByUser);
+});
 
-countCompletedTasksByUser('https://jsonplaceholder.typicode.com/todos');
