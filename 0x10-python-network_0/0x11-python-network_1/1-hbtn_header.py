86c346c9f2c4e0874cc67410741118ed77ec42e6
stewie-gil <gilliganstewie7@gmail.com>
2023-05-03 06:41:35 -0700

    removed

--- 0x10-python-network_0/0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Python script takes a url."""
-
-from urllib import request
-from sys import argv
-
-if __name__ == "__main__":
-    with request.urlopen(argv[1]) as page:
-        print(page.getheader("X-Request-Id"))
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const req = require('request');
-const u = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-req.get(u, (err, res, body) => {
-  if (err) console.log(err);
-  else console.log(JSON.parse(body).title);
-});
