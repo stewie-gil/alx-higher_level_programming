fdd5e15745de25bd00d8fd8d1a162c1c210eff7b
stewie-gil <gilliganstewie7@gmail.com>
2023-05-23 07:35:30 -0700

    fixed semi standard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (error, data) => {
-    if(error){
-	console.error(error);
-    } else {
-	console.log(data);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
 });
